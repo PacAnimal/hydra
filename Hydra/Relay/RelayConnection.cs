3c14da7c8feba0e9e4e471db207e5272dff4558d
Matteo Veglia <1258632+matteoveglia@users.noreply.github.com>
2026-08-25 13:18:07 +0100

    fix: use relay socket connector and adjust TUI navigation layout
    (cherry picked from commit 9b778667b314361ad66eddbe7be5f569d15c3e8e)

--- Hydra/Relay/RelayConnection.cs
+++ Hydra/Relay/RelayConnection.cs
@@ -296,8 +296,7 @@ public class RelayConnection(IHydraProfile profile, ILogger<RelayConnection> log
         {
             ConnectCallback = async (ctx, cancel) =>
             {
-                var socket = new Socket(SocketType.Stream, ProtocolType.Tcp) { NoDelay = true };
-                await socket.ConnectAsync(ctx.DnsEndPoint, cancel);
+                var socket = await RelaySocketConnector.ConnectAsync(ctx.DnsEndPoint, cancel);
                 return new NetworkStream(socket, ownsSocket: true);
             }
         };
